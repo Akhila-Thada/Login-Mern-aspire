62e4524b27f5b34801b465fa7ca889e96d885cf4
Akhila-Thada <akhila.thada@gmail.com>
2025-09-22 15:43:27 +0530

    Remove node_modules from repo

--- backend/server.js
+++ backend/server.js
@@ -127,6 +127,8 @@ app.post("/login", async (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('server is running on port 3000')
-})
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on ${process.env.PORT || 3000}`);
+});
